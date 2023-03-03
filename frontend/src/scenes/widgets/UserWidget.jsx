c14f7db739caa30076bab7be39f9c13d524c7312
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-03-03 15:00:52 +0530

    home page complete

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -93,6 +93,8 @@ const UserWidget = ({ userId, picturePath }) => {
         </Box>
       </Box>
 
+      <Divider />
+
       {/* THIRD ROW */}
       <Box p="1rem 0">
         <FlexBetween mb="0.5rem">
@@ -108,6 +110,8 @@ const UserWidget = ({ userId, picturePath }) => {
           </Typography>
         </FlexBetween>
       </Box>
+
+      <Divider />
 
       {/* FORTH ROW */}
       <Box p="1rem 0">
